b97bc9718c089327d7ef34d2d2c22db648f4b3bf
vmakarov28 <vmakarov28@students.d125.org>
2025-06-03 14:10:56 -0500

    Update tensorflowtest.py

--- Setup/tensorflowtest.py
+++ Setup/tensorflowtest.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-print(tf.__version__)  # Should be 2.19.0
-print(Sequential)  # Should show <class 'tensorflow.python.keras.models.Sequential'>
+print(tf.__version__)  # Should print 2.19.0
+print(Sequential)  # Should print <class 'tensorflow.python.keras.models.Sequential'>
