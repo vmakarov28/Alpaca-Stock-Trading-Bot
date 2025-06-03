a6958a8da7ca0a003ddac842c5842489248f1107
vmakarov28 <vmakarov28@students.d125.org>
2025-06-03 14:09:55 -0500

    Delete tensorflowtest.py

--- tensorflowtest.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-print(tf.__version__)  # Should be 2.19.0
-print(Sequential)  # Should show <class 'tensorflow.python.keras.models.Sequential'>
